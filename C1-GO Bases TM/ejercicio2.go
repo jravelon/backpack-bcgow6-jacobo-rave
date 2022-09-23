a10d23ed9b7e22370ff09e324bc604e669097e5b
jravelon <jacobo.rave@mercadolibre.com.co>
2022-09-23 12:09:37 -0500

    C1-GO Bases TM

--- C1-GO Bases TM/ejercicio2.go
+++ C1-GO Bases TM/ejercicio2.go
@@ -6,5 +6,5 @@ func main() {
 	var temperatura float64 = 25.5
 	var humedad int = 60
 	var presion int = 1000
-	fmt.Print(temperatura, humedad, presion)
+	fmt.Print(temperatura, ", ", humedad, ", ", presion)
 }
