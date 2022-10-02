73e69bb846adcd6e0f74796698ac2f3efd9f3cab
jravelon <jacobo.rave@mercadolibre.com.co>
2022-10-02 04:40:42 -0500

    improving practices

--- C2-GO Bases TM/Ejercicio3.go
+++ C2-GO Bases TM/Ejercicio3.go
@@ -1,44 +1,32 @@
 package main
 
-import "fmt"
-
-const (
-	categA = "A"
-	categB = "B"
-	categC = "C"
+import (
+	"fmt"
 )
 
-func calcTipo(tipo string) float64 {
-	switch tipo {
-	case categA:
-		return 3000.0
-	case categB:
-		return 1500.0
-	case categC:
-		return 1000.0
-	}
-	return 0
+const (
+	categoriaA = "A"
+	categoriaB = "B"
+	categoriaC = "C"
+)
+
+func conversor(minutos int) float64 {
+	return float64(minutos) / 60
 }
 
-func calcAumento(tipo string) float64 {
-	switch tipo {
-	case categA:
-		return 0.5
-	case categB:
-		return 0.2
-	case categC:
-		return 0
+func salario(minutos int, categoria string) float64 {
+	horas := conversor(minutos)
+	if categoria == categoriaC {
+		return horas * 1000
+	} else if categoria == categoriaB {
+		salary := horas * 1500
+		return salary + (salary * 0.2)
+	} else {
+		salary := horas * 3000
+		return salary + (salary * 0.5)
 	}
-	return 0
-}
-
-func calcSalario(minutos int, tipo string) float64 {
-	horas := float64(minutos) / 60
-	neto := horas * calcTipo(tipo)
-	salarioFinal := neto + neto*calcAumento(tipo)
-	return salarioFinal
 }
 
 func main() {
-	fmt.Println(calcSalario(60, categC))
+	fmt.Println(salario(60, categoriaB))
 }
