56c5e0c15623a86a67b927364e9af2f9d4aba952
jravelon <jacobo.rave@mercadolibre.com.co>
2022-10-03 13:41:41 -0500

    finish Tm1

--- GO-Web/C1-GOWeb-TM/main.go
+++ GO-Web/C1-GOWeb-TM/main.go
@@ -4,13 +4,66 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Fecha struct {
+	Año int
+	Mes string
+	Día int
+}
+type Users struct {
+	Id            int
+	Nombre        string
+	Apellido      string
+	Email         string
+	Edad          int
+	Altura        float64
+	Activo        bool
+	FechaCreacion Fecha
+}
+
+func GetAll(ctx *gin.Context) {
+	UsersEj := []Users{
+		{
+			Id:       1212,
+			Nombre:   "J",
+			Apellido: "R",
+			Email:    "j.r@mercadolibre.com.co",
+			Edad:     19,
+			Altura:   1.85,
+			Activo:   true,
+			FechaCreacion: Fecha{
+				Año: 2022,
+				Mes: "Octubre",
+				Día: 3,
+			},
+		},
+		{
+			Id:       1212,
+			Nombre:   "J",
+			Apellido: "R",
+			Email:    "j.r@mercadolibre.com.co",
+			Edad:     19,
+			Altura:   1.85,
+			Activo:   true,
+			FechaCreacion: Fecha{
+				Año: 2022,
+				Mes: "Octubre",
+				Día: 3,
+			},
+		},
+	}
+	ctx.JSON(200, UsersEj)
+}
+
 func main() {
+
 	router := gin.Default()
 
-	router.GET("/hola", func(ctx *gin.Context) {
+	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "Hola Jacobo",
 		})
 	})
+
+	router.GET("/products", GetAll)
 	router.Run()
 }
