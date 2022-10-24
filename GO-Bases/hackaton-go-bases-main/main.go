70840a54826e82cb4b275b52a5352223c7f59097
jravelon <jacobo.rave@mercadolibre.com.co>
2022-10-24 12:31:32 -0500

    update

--- GO-Bases/hackaton-go-bases-main/main.go
+++ GO-Bases/hackaton-go-bases-main/main.go
@@ -8,13 +8,63 @@ import (
 )
 
 func main() {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Print("Ocurrio un error: ", err)
+		}
+	}()
+
+	var ticket []service.Ticket
+
 	file := file.File{"tickets.csv"}
-	tickets, _ := file.Read()
-	booking := service.NewBookings(tickets)
-	t, _ := booking.Read(2)
-	booking.Delete(2)
-	booking.Delete(3)
-	_, err := booking.Read(2)
-	fmt.Println(t)
-	fmt.Println(err)
+	data, err := file.Read()
+	if err != nil {
+		panic(err)
+	}
+
+	ticket = append(ticket, data...)
+
+	fmt.Print("Cargando tickets")
+	fmt.Print("......\n")
+
+	booking := service.NewBookings(ticket)
+	fmt.Printf("hay %d tickets", len(ticket))
+	fmt.Print("......\n")
+
+	newTicket := service.Ticket{
+		Id:          1222,
+		Names:       "Joao da silva",
+		Email:       "Joao@email.com",
+		Destination: "Seul",
+		Date:        "12:3",
+		Price:       1000,
+	}
+	a, err := booking.Create(newTicket)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Nuevo ticket guardado ")
+	fmt.Print(a, "......\n")
+
+	t, err := booking.Read(2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("ticket leido %v", t)
+	fmt.Print("......\n")
+
+	err = file.Write(&newTicket)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("cambios guardados.........\n")
+
+	b, err := booking.Read(1222)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("ticket leido %v", b)
+	fmt.Print("......\n")
+
 }
